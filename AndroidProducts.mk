44e8cbe2c3b3bddc2f4cb9c92c80d340273ebf9c
Jamie Falconer <mrfluffyoven@gmail.com>
2024-09-27 09:20:11 +0000

    initial commit

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for samsung SM-X216B (gta9p)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_gta9p.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_gta9p-user \
+    twrp_gta9p-userdebug \
+    twrp_gta9p-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for samsung SM-X216B (gta9p)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo twrp_gta9p-user
+add_lunch_combo twrp_gta9p-userdebug
+add_lunch_combo twrp_gta9p-eng
